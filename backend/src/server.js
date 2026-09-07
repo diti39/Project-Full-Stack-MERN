79e634cd8d00e668f97cd15eb3f67a55035d8da5
Ardit Shehu <aarditshehu@gmail.com>
2026-09-07 20:59:16 +0200

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,10 +10,9 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
-app.use(cors({origin: CLIENT_URL
-})); // Enable CORS for all routes 
+app.use(cors({origin: "http://localhost:5173"
+})); // Enable CORS for all routes
 
 
 app.use(express.json()); // Middleware to parse JSON bodies
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "http://localhost:5001/api",
 });
 
 api.interceptors.request.use(
